60b9edb966a830b6f5fe78c5dc467020c04acdfc
massbchou <70417919+massbchou@users.noreply.github.com>
2023-11-17 16:40:58 -0500

    Add Rabbit Mitosis and Death
    Rabbits now split upon eating enough and die when they don't.

--- Assets/Scripts/Rabbit.cs
+++ Assets/Scripts/Rabbit.cs
@@ -8,7 +8,8 @@ public class Rabbit : Animal
     GameObject _currentTarget = null;
     Vector3 _currentTargetPosition;
     [SerializeField] float _eatingDistance = 1f;
-
+    [SerializeField] float _maxBelly = 10f;
+    private float belly;
     bool wandering = false;
 
     // Start is called before the first frame update
@@ -17,6 +18,7 @@ public class Rabbit : Animal
         wandering = false;
         _currentTarget = null;
         base.Start();
+        belly = _maxBelly;
     }
 
     // Update is called once per frame
@@ -36,7 +38,6 @@ public class Rabbit : Animal
         if (_currentTarget == null && !wandering)
         {
             wandering = true;
-            //TODO: FIX THIS MATH TO BE RELATIVE TO THE RABBIT
             _currentTargetPosition = new Vector3(transform.position.x + Random.Range(-1f, 1f) * _senseRadius, transform.position.y, transform.position.z + Random.Range(-1f, 1f) * _senseRadius);
             _agent.SetDestination(_currentTargetPosition);
         }
@@ -45,9 +46,34 @@ public class Rabbit : Animal
         float dist = (transform.position - _currentTargetPosition).magnitude;
         if (dist < _eatingDistance)
         {
+            if (!wandering)
+            {
+                //Grow in size by 11%
+                transform.localScale *= 1.11f;
+                //feed 
+                belly = _maxBelly;
+            }
             wandering = false;
             if(_currentTarget != null) Destroy(_currentTarget);
             _currentTarget = null;
+            
+        }
+        
+        //if scale reaches 2, split into two new rabbits
+        if(transform.localScale.z > 2f)
+        {
+            transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+            GameObject newRabbit = Instantiate(gameObject, transform.position + new Vector3(1f, 0f, 0f), Quaternion.identity);
+            newRabbit.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+            belly /= 2;
+        }
+        
+        //decrease belly by time
+        belly -= Time.deltaTime;
+        
+        if (belly < 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
