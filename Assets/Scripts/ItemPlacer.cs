ee2124e57db7f1a37c794385c383559abf73232a
Emerson Lewiecki <elewiecki1@verizon.net>
2023-12-10 14:43:20 -0500

    Added crosshair with changing colors

--- Assets/Scripts/ItemPlacer.cs
+++ Assets/Scripts/ItemPlacer.cs
@@ -17,7 +17,8 @@ public class ItemPlacer : MonoBehaviour
     [SerializeField] GameObject _fox;
     [SerializeField] GameObject _plant;
 
-    [SerializeField] LayerMask ground;
+    [SerializeField] GameObject crosshair;
+    Color crosshairColor = Color.green;
 
     GameObject _currentSelection;
 
@@ -38,16 +39,19 @@ public class ItemPlacer : MonoBehaviour
     void SelectPlant()
     {
         _currentSelection = _plant;
+        crosshairColor = Color.green;
     }
 
     void SelectRabbit()
     {
         _currentSelection = _rabbit;
+        crosshairColor = new Color(0.7f, 0.7f, 0.7f);
     }
 
     void SelectFox()
     {
         _currentSelection = _fox;
+        crosshairColor = new Color(1.0f, 0.64f, 0.0f);
     }
 
     public void EnablePlantButton()
@@ -66,20 +70,39 @@ public class ItemPlacer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //RAYCAST FOR CROSSHAIRS
+        Ray raycast = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(raycast, out hit, 1000))
+        {
+            crosshair.transform.position = hit.point + new Vector3(0, 0.4f, 0);
+
+            if (_currentSelection == null || hit.collider.gameObject.CompareTag(_currentSelection.tag))
+            {
+                crosshair.GetComponent<SpriteRenderer>().color = Color.red;
+            }
+            else
+            {
+                crosshair.GetComponent<SpriteRenderer>().color = crosshairColor;
+            }
+        }
+
+
+        //RAYCAST FOR ITEM PLACEMENT
         if (Input.GetKeyDown(KeyCode.Mouse0) && _currentSelection != null)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitData;
             if (Physics.Raycast(ray, out hitData, 1000))
             {
-                if (hitData.collider.gameObject.CompareTag(_currentSelection.tag))
+                //don't place on top of other object of same type
+                if (!hitData.collider.gameObject.CompareTag(_currentSelection.tag))
                 {
-                    return;
+                    Vector3 worldPosition = hitData.point;
+                    Instantiate(_currentSelection, worldPosition + Vector3.up * 0.1f, Quaternion.identity);
                 }
-                Vector3 worldPosition = hitData.point;
-                Instantiate(_currentSelection, worldPosition + Vector3.up * 0.1f, Quaternion.identity);
-
             }
         }
+
     }
 }
